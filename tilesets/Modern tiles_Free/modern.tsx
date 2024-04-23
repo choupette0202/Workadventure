9c11cea19ee384ac9d1a9bf6574607c72dad9d66
choupette0202 <schuwercamille@gmail.com>
2024-04-23 16:09:06 +0200

    add test

---
+++ tilesets/Modern tiles_Free/modern.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="modern" tilewidth="32" tileheight="32" tilecount="3248" columns="58">
+ <image source="free_overview.png" width="1886" height="1792"/>
+</tileset>
---
+++ tilesets/Super_Retro_World_Interior_pack_week42/Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Test" tilewidth="32" tileheight="32" tilecount="1728" columns="72">
+ <image source="atlas_48x.png" width="2304" height="768"/>
+</tileset>
